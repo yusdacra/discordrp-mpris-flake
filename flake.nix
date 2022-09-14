770a8170a953d7fea82a4b2f8cefab531c956bb0
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-09-15 02:22:58 +0300

    make it work

--- flake.nix
+++ flake.nix
@@ -5,10 +5,23 @@
   inputs.src.url = "github:FichteFoll/discordrp-mpris";
   inputs.src.flake = false;
 
-  outputs = inp:
+  outputs = inp: let
+    system = "x86_64-linux";
+    pkgs = inp.nixpkgs.legacyPackages.${system};
+  in
     inp.dream2nix.lib.makeFlakeOutputs {
-      systemsFromFile = ./nix_systems;
+      systems = [system];
       config.projectRoot = ./.;
       source = inp.src;
+      packageOverrides = {
+        main.add-lib.overrideAttrs = old: {
+          nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.makeWrapper];
+          postInstall = ''
+            ${old.postInstall or ""}
+            wrapProgram $out/bin/discordrp-mpris \
+              --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [pkgs.dbus]}"
+          '';
+        };
+      };
     };
 }
