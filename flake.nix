c5f5a287f4f8affd1efa6b82a21cea53e60cfa8d
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-09-15 02:09:09 +0300

    follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.dream2nix.url = "github:nix-community/dream2nix";
+  inputs.dream2nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.src.url = "github:FichteFoll/discordrp-mpris";
   inputs.src.flake = false;
 
