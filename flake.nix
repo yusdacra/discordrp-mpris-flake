6111c2035efde0741f1fc99b552d32a24f8f5a1d
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-11-17 15:57:27 +0300

    use newer fork

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.dream2nix.url = "github:nix-community/dream2nix";
   inputs.dream2nix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.src.url = "github:FichteFoll/discordrp-mpris";
+  inputs.src.url = "github:SergSel2006/discordrp-mpris-albumart";
   inputs.src.flake = false;
 
   outputs = inp: let
